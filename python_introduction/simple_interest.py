2f6dd5e9a37cf2b1e82962a997a701ec549340b7
ivy <ivngigiy@gmail.com>
2024-12-13 12:48:06 +0300

    add a file simple_interest.py with simple interest calculation

---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,10 @@
+# define the variables to calculate simple interest
+principal = 1000
+rate = 0.05
+time = 3
+
+# calculate simple interest
+interest = principal * rate * time
+
+# print results in human readable format
+print(f"the simple interest is : {interest}")
