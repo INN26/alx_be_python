f22b1d6c7eab561045105a477029ef37aefca19e
ivy <ivngigiy@gmail.com>
2024-12-13 13:14:01 +0300

    add new file rectangle_area.py

---
+++ python_introduction/rectangle_area.py
@@ -0,0 +1,9 @@
+# Define two variables
+length = 10
+width = 5
+
+# Calculate the area of the rectangle 
+area = length * width
+
+# print the calculated area 
+print(f"the area of the triangle is : {area}")
