8f850d4d3115899a700b0ade4b38a55d6dbbb5ce
ivy <ivngigiy@gmail.com>
2025-01-09 22:52:05 +0300

    Add a script that implements a simple interface for managing a shopping list

--- fns_and_dsa/shopping_list_manager.py
+++ fns_and_dsa/shopping_list_manager.py
@@ -10,7 +10,7 @@ def display_menu():
 
 #Prompt the user for the item name and append it
 def add_item():
-    item =input("\"Enter the item to add: \"")
+    item =input("[\"]Enter the item to add: [\"]")
     shopping_list.append(item)
     print(f"{item} has been added to the shopping list.")
 
