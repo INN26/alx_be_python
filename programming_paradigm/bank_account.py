2518b8015d7c24525ee27f8d4ed25357a2f9c893
ivy <ivngigiy@gmail.com>
2025-01-18 15:09:53 +0300

    add a file

---
+++ programming_paradigm/bank_account.py
@@ -0,0 +1,38 @@
+#define a class bank account
+class BankAccount:
+
+     #initialise an account balance
+      def __init__(self, initial_balance = 0):
+            
+      #initialise the bank account   
+            self.account_balance = initial_balance
+      def deposit(self, amount):
+      #deposit amount in the bank account
+            if amount > 0:
+                  self.account_balance += amount
+            else:
+                  print("deposit amount must be positve")
+                  
+      def withdraw(self, amount):
+      #withdaraw amount in the bank account if funds are sufficient
+            if amount > self.account_balance:
+                 return False
+            
+            else:
+                  self.account_balance -= amount
+                  return True
+                              
+            
+      def display_balance(self):  
+      #print the current balance in a user-friendly format. 
+            print(f"account_balance : ${self.account_balance :.2f}")   
+
+
+ 
+
+
+
+
+ 
+      
+       
