ed10dc89bed97ee3e6876306a5912d5fe9ce9136
ivy <ivngigiy@gmail.com>
2025-01-18 18:23:31 +0300

    add file

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -26,6 +26,7 @@ from test_simple_calculator import SimpleCalculator
 
 class TestSimpleCalculator(unittest.TestCase):
 
+
     def setUp(self):
         """Set up the SimpleCalculator instance before each test."""
         self.calc = SimpleCalculator()
