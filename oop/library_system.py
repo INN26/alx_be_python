ab6e51a7b5da3f0112a7cc038fe6bda0cae71eb7
ivy <ivngigiy@gmail.com>
2025-01-21 14:49:55 +0300

    add a file

--- oop/library_system.py
+++ oop/library_system.py
@@ -4,21 +4,29 @@ class Book:
         self.title = title
         self.author = author
     def __str__(self):
-        return  f"{self.title} by  {self.author}"
+        return f"Book: {self.title} by {self.author}"
          
 #derived classes
 class EBook(Book):
         def __init__(self, title, author, file_size):
              super().__init__(title, author)
              self.file_size = file_size
+
+
         def __str__(self):
              return f"EBook: {super().__str__()}, File Size: {self.file_size}KB"
+        
+
 class PrintBook(Book):
      def __init__(self, title, author, page_count):
           super().__init__(title, author)
           self.page_count = page_count
+
+
      def __str__(self):
              return f"PrintBook: {super().__str__()}, Page Count: {self.page_count}"
+     
+
 class Library:
          def __init__ (self):
               self.books = []
